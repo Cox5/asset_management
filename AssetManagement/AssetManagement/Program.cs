683677e2411212e372c2cb20c27d31e202390df2
Milan Masnikosa <milan.masnikosa@gmail.com>
2018-05-16 13:58:41 +0200

    Inicijalni projekat

---
+++ AssetManagement/AssetManagement/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
