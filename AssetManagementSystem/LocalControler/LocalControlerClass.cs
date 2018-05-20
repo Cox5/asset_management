b204cb9485a76723fd3d53a64093d09f07fdf2df
Nemanja1996 <nemanjav1996@hotmail.com>
2018-05-20 16:48:37 +0200

    LocalControler doradjivanje

--- AssetManagementSystem/LocalControler/LocalControlerClass.cs
+++ AssetManagementSystem/LocalControler/LocalControlerClass.cs
@@ -2,8 +2,10 @@
 using LocalDevice;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
 
@@ -38,6 +40,33 @@ namespace LocalControler
 
             doc.AppendChild(root);
             doc.Save(@"..\..\..\Communication\" + name + id + ".xml");
+        }
+
+        public void SendData()
+        {
+            FileInfo[] dllFiles = null;
+            do
+            {
+                try
+                {
+                    DirectoryInfo di = new DirectoryInfo(@"../../../Communication/");
+                    dllFiles = di.GetFiles("AMS.xml");
+
+                    if (dllFiles.Length < 1)
+                    {
+                        Console.WriteLine("Greska pri pretrazi fajla: Fajl sa datim nazivom ne postoji...");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.Write($"Greska pri citanju foldera: Comunication direktorijum je prazan...");
+                    Thread.Sleep(2500);
+                }
+            }
+            while (dllFiles==null);
+
+
+            Console.WriteLine("Sledi slanje podataka u fajl...");
         }
     }
 }
