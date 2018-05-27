dfe42321f2eb51c8bf567156015cff3714d057f2
Milan Masnikosa <milan.masnikosa@gmail.com>
2018-05-27 19:07:29 +0200

    Dodavanje AMSClass, irpsavljanje bagova i pocetak binding-a

--- AssetManagementSystem/AMS/MainWindow.xaml.cs
+++ AssetManagementSystem/AMS/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using AMS_Common;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,20 @@ namespace AMS
     /// </summary>
     public partial class MainWindow : Window
     {
+        //public ObservableCollection<>
+        AMSClass ams = new AMSClass();
+        public Dictionary<int, List<Tuple<string, List<Tuple<string, ILocalDevice>>>>> AMSDatabase { get; set; }
+        public static BindingList<Dictionary<int, List<Tuple<string, List<Tuple<string, ILocalDevice>>>>>> DevicesBindingList { get; set; }
+        
+
         public MainWindow()
         {
             InitializeComponent();
+            if (DevicesBindingList == null)
+            {
+                //DevicesBindingList = new BindingList<Dictionary<int, List<Tuple<string, List<Tuple<string, ILocalDevice>>>>>>(AMSDatabase);
+            }
+            DataContext = this;
         }
     }
 }
